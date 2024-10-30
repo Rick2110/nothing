8dc5f4dbee76ce7b8d9e71326f069eda0f45dd74
Henrique <96449635+Rick2110@users.noreply.github.com>
2024-10-30 09:39:32 -0300

    Add files via upload

---
+++ exercicios.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+/*Nome:Henrique Oliveira da Silva*/
+
+
+public class exercicios {
+	public static void main(String[] args) {
+		/*Scanner sc = new Scanner(System.in);
+		double altura,comprimento,latas,tamanho;
+		latas = 0;
+		
+		System.out.println("Qual a altura da parede:");
+		altura = sc.nextInt();
+		
+		System.out.println("Qual o comprimento da parede:");
+		comprimento = sc.nextInt();
+		
+		tamanho = altura * comprimento;
+		
+		for (int i = 1; i < tamanho;) {
+			tamanho = tamanho - 1.6;
+			latas = latas + 1;
+		}
+		System.out.println("São necessarios em "+latas+" latas");
+	}*/
+	
+		Scanner sc = new Scanner(System.in);
+		double maioraltura,menoraultura,medialtura,mulheres
+		
+		double[] altura = {1.80,1.75,1.86,1.72,1.65,1.55,1.72,1.52,1.90,1.92}
+		
+		String[] generos = {"M","M","F","M","F","F","M","M","M","F"}
+		
+		for (int i = 0;i < altura.length;i ++) {
+			if altura[i] > 1.90;
+				System.out.println("A maior altura é"+altura[i]);
+			
+		}
+	
+
+}
